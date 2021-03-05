63306e23294ff383e3916521a7963e2de67abbd9
Leigh Bitossi <leeroy77@gmailcom>
2021-03-05 11:02:27 +0900

    Added body-parser, cors

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -6,11 +6,15 @@ function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch('http://localhost:8081/analysis')
     .then(res => res.json())
     .then(function(res) {
+        console.log("response from /analysis",res);
         document.getElementById('results').innerHTML = res.message
     })
 }
+
+// post data function - send data to server
+
 
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -3,31 +3,41 @@ dotenv.config();
 const fetch = require('node-fetch');
 var path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
-var myapi = new meaningCloud ({
-    application_key: process.env.API_KEY
+const mockAPIResponse = require('./mockAPI.js');
+const { response } = require('express');
+//var myapi = new meaningCloud ({
+    //application_key: process.env.API_KEY
 
-});
+//});
 
-const app = express()
+const app = express();
 
-app.use(express.static('dist'))
+/* Middleware */
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-console.log(__dirname)
+/* Cors */
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.static('dist'));
+
+console.log(__dirname);
 
 app.get('/', function (req, res) {
      res.sendFile('dist/index.html')
     //res.sendFile(path.resolve('src/client/views/index.html'))
-})
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
-})
+});
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
-})
+});
 
 app.post('/analysis', function (req, res) {
     const API_KEY = process.env.API_KEY;
@@ -39,5 +49,15 @@ app.post('/analysis', function (req, res) {
         headers: {
             "Content-Type": "application/JSON",
         }
+    }).then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log("Meaningcloud data", data);
+        res.send({
+            score_tag: data.score_tag,
+            agreement: data.agreement,
+        })
+
     })
-})
+
+});
