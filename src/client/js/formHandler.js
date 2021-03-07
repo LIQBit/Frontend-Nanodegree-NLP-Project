d0e9bc79ce18dcbffec1d55da738577b6be11c6a
Leigh Bitossi <leeroy77@gmailcom>
2021-03-07 21:35:41 +0900

    changes to server and formHandler

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -6,15 +6,39 @@ function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/analysis')
-    .then(res => res.json())
-    .then(function(res) {
-        console.log("response from /analysis",res);
-        document.getElementById('results').innerHTML = res.message
-    })
+    postText(formText)
+    
 }
 
 // post data function - send data to server
+const postText = (url = '') => {
+    const response = fetch('http://localhost:8081/analysis', {
+        method: 'POST',
+        credentials: 'same-origin',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: url,
+    })
+        .then(res => res.json())
+        .then((data) => updateUI(data))
+        .catch((error) => {
+            console.log('error', error);
+        });
+            function updateUI(data) {
+                console.log(data);
+                //just console.log data for now
 
+            }
+    
+
+
+
+
+
+
+
+}
 
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,15 @@
 const dotenv = require('dotenv');
 dotenv.config();
+const baseURL = "https://api.meaningcloud.com/sentiment-2.1?";
+const API_KEY = process.env.API_KEY;
+
 const fetch = require('node-fetch');
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js');
-const { response } = require('express');
-//var myapi = new meaningCloud ({
-    //application_key: process.env.API_KEY
 
-//});
+//Require Express
+const { response } = require('express');
 
 const app = express();
 
@@ -23,41 +24,29 @@ app.use(cors());
 
 app.use(express.static('dist'));
 
-console.log(__dirname);
 
-app.get('/', function (req, res) {
-     res.sendFile('dist/index.html')
-    //res.sendFile(path.resolve('src/client/views/index.html'))
-});
-
-// designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
+// Designated port for incoming requests
+const port =8081;
+app.listen(port, function () {
     console.log('Example app listening on port 8081!')
 });
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+//Get route
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
 });
 
-app.post('/analysis', function (req, res) {
-    const API_KEY = process.env.API_KEY;
-    const articleUrl = req.body.articleUrl;
-    const baseURL = "https://api.meaningcloud.com/sentiment-2.1?key=${API_KEY}&lang=en&model=general&url=${articleUrl}";
+//Post route
+app.post('/analysis', async (req, res) => {
+    console.log(req.body);
 
-    fetch(baseURL, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/JSON",
-        }
-    }).then((response) => {
-        return response.json();
-    }).then((data) => {
-        console.log("Meaningcloud data", data);
-        res.send({
-            score_tag: data.score_tag,
-            agreement: data.agreement,
-        })
-
-    })
+    const response = await fetch(`${baseURL}${API_KEY}&of=json&lang=en&txt=${req.body.txt}&model=general&url=${req.body.url}`);
+    try {
+        const data = await response.json();
+        res.send(data)
+        res.json()
+    } catch(error) {
+        console.log('error', error);
+    }
 
 });
